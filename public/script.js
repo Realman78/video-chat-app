0b4aa3c70cb7efac76272a79df118bbbef4664f9
Realman78 <marin.dedic@me.com>
2021-06-13 00:57:26 +0200

    revert na port 443

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    port: 4000
+    port: 443
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
